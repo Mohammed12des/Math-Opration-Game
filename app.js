c06d892452653bd80c9c586fec045d7d929937ce
Mohammed12des <hamood12des@gmail.com>
2024-07-06 17:36:53 +0300

    second

---
+++ app.js
@@ -0,0 +1,15 @@
+const qus = document.querySelector('#pro');
+
+const btnEasy =document.querySelector('#easy');
+const btnmed =document.querySelector('#med');
+const btnhard =document.querySelector('#hard');
+const btnveryHard =document.querySelector('#Veryhard');
+
+
+
+
+
+btnEasy.addEventListener('click')
+btnmed.addEventListener('click')
+btnhard.addEventListener('click')
+btnveryHard.addEventListener('click')
