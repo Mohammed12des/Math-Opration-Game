5bc18b183fdda41bf471c0add3c068fd2fd067b1
Mohammed12des <hamood12des@gmail.com>
2024-07-07 15:33:06 +0300

    third

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const qus = document.querySelector('#pro');
-
 const btnEasy =document.querySelector('#easy');
 const btnmed =document.querySelector('#med');
 const btnhard =document.querySelector('#hard');
 const btnveryHard =document.querySelector('#Veryhard');
+const sloution = document.querySelector('#text');
 
 
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+btnEasy.addEventListener('click',()=>{
+qus.innerText = `${getRandomInt(30)} + ${getRandomInt(20)}`
+if(sloution.innerText){
 
-
-btnEasy.addEventListener('click')
-btnmed.addEventListener('click')
-btnhard.addEventListener('click')
-btnveryHard.addEventListener('click')
+}
+})
+btnmed.addEventListener('click',()=>{
+    qus.innerText = `${getRandomInt(40)} - ${getRandomInt(20)}`} )
+btnhard.addEventListener('click', ()=>{
+    qus.innerText = `${getRandomInt(30)} + ${getRandomInt(20)} * ${getRandomInt(10)}` })
+btnveryHard.addEventListener('click',()=>{
+    qus.innerText = `${getRandomInt(30)} + ${getRandomInt(20)} * ${getRandomInt(10)} / ${getRandomInt(10)}`})
